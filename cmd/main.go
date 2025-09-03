502765abf1ac94050d8a78ad7fa24395aeb19f33
boretsotets <cachow@bk.ru>
2025-09-03 20:00:47 +0300

    реализовано хеширование паролей

--- cmd/main.go
+++ cmd/main.go
@@ -27,13 +27,21 @@ func main() {
 		log.Fatal("error while creating table: ", err)
 	}
 
+	_, err = db.Exec(context.Background(), 
+	"CREATE TABLE IF NOT EXISTS users (Email text PRIMARY KEY, Name TEXT, Password TEXT, Token TEXT)")
+	if err != nil {
+		log.Fatal("error while creating table: ", err)
+	}
+
 	app := &handlers.App{DB: db}
 
 	router := gin.Default()
+	router.POST("/register", app.SignUp)
+	router.POST("/login", app.SignIn)
 	router.GET("/todos", app.HandlerGet)
-	router.POST("/todos", handlers.HandlerPost)
-	router.PUT("/todos/:id", handlers.HandlerUpdate)
-	router.DELETE("todos/:id", handlers.HandlerDelete)
+	router.POST("/todos", app.HandlerPost)
+	router.PUT("/todos/:id", app.HandlerUpdate)
+	router.DELETE("todos/:id", app.HandlerDelete)
 	router.Run("localhost:8080")
 
 }
--- internal/handlers.go
+++ internal/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"context"
@@ -10,6 +9,8 @@ import (
 	"net/http"
 	"encoding/json"
 	"strconv"
+	"crypto/sha256"
+	"encoding/hex"
 )
 //DB_HOST=localhost
 //DB_PORT=5432
@@ -38,7 +39,12 @@ type App struct {
 
 func (a *App) HandlerGet(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	
+
+	if authtoken := c.GetHeader("Authorization"); authtoken == "" {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message" : "unauthorized"})
+		return
+	}
+
 	rows, err := a.DB.Query(context.Background(), "SELECT Id, Title, Description FROM tasks")
 	if err != nil {
 		log.Fatalf("Query failed: %v", err)
@@ -49,108 +55,132 @@ func (a *App) HandlerGet(c *gin.Context) {
 	for rows.Next() {
 		var id int
 		var title, description string
-		rows.Scan(&id, &title, &description)		
+		if err := rows.Scan(&id, &title, &description); err != nil {
+			log.Fatalf("Rows mapping error: %v", err)
+		}	
 		tasks = append(tasks, gin.H{"id": id, "title": title, "description": description})
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatalf("Rows iteration error: %v", err)
 	}
 
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
-type RespondUnauthorized struct {
-	Message string`json:"message"`
-}
-
-func HandlerPost(c *gin.Context) {
+func (a *App) HandlerPost(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 
 	if authtoken := c.GetHeader("Authorization"); authtoken == "" {
-		var data RespondUnauthorized
-		data.Message = "unauthorized"
-		c.IndentedJSON(http.StatusUnauthorized, data)	
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message" : "unauthorized"})
+		return
+	} 
+	
+	var newtask JsonTaskPost
+	err := json.NewDecoder(c.Request.Body).Decode(&newtask)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Error decoding JSON")
 	} else {
-		var newtask JsonTaskPost
-		err := json.NewDecoder(c.Request.Body).Decode(&newtask)
-		if err != nil {
-			c.String(http.StatusBadRequest, "Error decoding JSON")
-		} else {
-			conn, err := pgx.Connect(context.Background(), "postgres://postgres:secret@localhost:5432/postgres?sslmode=disable")
-			if err != nil {
-				log.Fatalf("Database connection failed: %v", err)
-			}
-			_, err = conn.Exec(context.Background(), 
-			"CREATE TABLE IF NOT EXISTS tasks (Id SERIAL PRIMARY KEY, Title TEXT, Description TEXT)")
-			if err != nil {
-				log.Fatalf("%v", err)
-			}
-
-			var curr_task Task
-			err = conn.QueryRow(context.Background(), "INSERT INTO tasks (Title, Description) VALUES ($1, $2) RETURNING (Id, $1, $2)", newtask.Title, newtask.Description).Scan(&curr_task)
-			if err != nil {
-				log.Fatalf("Rows iteration error: %v", err)
-			}
-			c.IndentedJSON(http.StatusOK, curr_task)
-		}
-	}
-
-}
-
-func HandlerUpdate(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-	if authtoken := c.GetHeader("Authorization"); authtoken == "" {
-		var data RespondUnauthorized
-		data.Message = "unauthorized"
-		c.Header("Content-Type", "application/json")
-		c.IndentedJSON(http.StatusUnauthorized, data)	
-	} else {
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			log.Fatalf("id conversion error: %v", err)
-		}
-		var newtask JsonTaskPost
-		err = json.NewDecoder(c.Request.Body).Decode(&newtask)
-
-		conn, err := pgx.Connect(context.Background(), "postgres://postgres:secret@localhost:5432/postgres?sslmode=disable")
-		if err != nil {
-			log.Fatalf("Database connection failed: %v", err)
-		}
-		_, err = conn.Exec(context.Background(), 
-		"CREATE TABLE IF NOT EXISTS tasks (Id SERIAL PRIMARY KEY, Title TEXT, Description TEXT)")
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
 		var curr_task Task
-		err = conn.QueryRow(context.Background(), "UPDATE tasks SET Title = $1, Description = $2 WHERE Id = $3 RETURNING (Id, Title, Description)", newtask.Title, newtask.Description, id).Scan(&curr_task)
+		err = a.DB.QueryRow(context.Background(), "INSERT INTO tasks (Title, Description) VALUES ($1, $2) RETURNING (Id, $1, $2)", newtask.Title, newtask.Description).Scan(&curr_task)
 		if err != nil {
 			log.Fatalf("Rows iteration error: %v", err)
 		}
 		c.IndentedJSON(http.StatusOK, curr_task)
-
-
 	}
+
+
 }
 
-func HandlerDelete(c *gin.Context) {
+func (a *App) HandlerUpdate(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 	if authtoken := c.GetHeader("Authorization"); authtoken == "" {
-		var data RespondUnauthorized
-		data.Message = "unauthorized"
-		c.Header("Content-Type", "application/json")
-		c.IndentedJSON(http.StatusUnauthorized, data)	
-	} else {
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			log.Fatalf("id conversion error: %v", err)
-		}
-		conn, err := pgx.Connect(context.Background(), "postgres://postgres:secret@localhost:5432/postgres?sslmode=disable")
-		if err != nil {
-			log.Fatalf("Database connection failed: %v", err)
-		}
-		_, err = conn.Exec(context.Background(), 
-		"CREATE TABLE IF NOT EXISTS tasks (Id SERIAL PRIMARY KEY, Title TEXT, Description TEXT)")
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
-		conn.QueryRow(context.Background(), "DELETE FROM tasks WHERE Id = $1", id)
-		c.Status(http.StatusNoContent)
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message" : "unauthorized"})
+		return
+	} 
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalf("id conversion error: %v", err)
 	}
+	var newtask JsonTaskPost
+	err = json.NewDecoder(c.Request.Body).Decode(&newtask)
+
+	var curr_task Task
+	err = a.DB.QueryRow(context.Background(), "UPDATE tasks SET Title = $1, Description = $2 WHERE Id = $3 RETURNING (Id, Title, Description)", newtask.Title, newtask.Description, id).Scan(&curr_task)
+	if err != nil {
+		log.Fatalf("Rows iteration error: %v", err)
+	}
+	c.IndentedJSON(http.StatusOK, curr_task)
+
+}
+
+func (a *App) HandlerDelete(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	if authtoken := c.GetHeader("Authorization"); authtoken == "" {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message" : "unauthorized"})
+		return
+	} 
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalf("id conversion error: %v", err)
+	}
+
+	a.DB.QueryRow(context.Background(), "DELETE FROM tasks WHERE Id = $1", id)
+	c.Status(http.StatusNoContent)
+	
+}
+
+func (a *App) SignUp(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	var userinfo map[string]string
+	err := json.NewDecoder(c.Request.Body).Decode(&userinfo)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Error decoding JSON")
+		log.Fatalf("error decoding JSON: %v", err)
+	}
+	password := userinfo["password"]
+
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	password = hashString
+	var token string
+	err = a.DB.QueryRow(context.Background(), 
+    "INSERT INTO users (Email, Name, Password, Token) VALUES ($1, $2, $3, $1) RETURNING Token", userinfo["email"], userinfo["name"], password).Scan(&token)
+    if err != nil {
+		log.Fatalf("Error querying row: %v", err)
+	}
+    c.IndentedJSON(http.StatusOK, gin.H{"token": token})
+
+}
+
+func (a *App) SignIn(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	var userinfo map[string]string
+	err := json.NewDecoder(c.Request.Body).Decode(&userinfo)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Error decoding JSON")
+		log.Fatalf("error decoding JSON: %v", err)
+	}
+
+	password := userinfo["password"]
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	password = hashString
+
+	var token string
+	err = a.DB.QueryRow(context.Background(), 
+    "SELECT Token FROm users WHERE Email = $1 AND Password = $2", userinfo["email"], password).Scan(&token)
+    if err != nil {
+		log.Fatalf("Error querying row: %v", err)
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"token": token})
+
 }
